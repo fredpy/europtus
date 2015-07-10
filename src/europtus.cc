0f171d6999589ef3d6eb11b5fa0c95a133195ca6
Frédéric Py <fredpy@gmail.com>
2015-07-10 16:10:40 +0000

    corrected missing closing }

--- src/europtus.cc
+++ src/europtus.cc
@@ -348,6 +348,7 @@ int main(int argc, char *argv[]) {
           std::ostringstream oss;
           oss<<"Failed to move europtus.log (attempts exceed "<<FILE_ATTEMPTS<<").";
           throw std::runtime_error(oss.str());
+        }
       }
     }
   }
